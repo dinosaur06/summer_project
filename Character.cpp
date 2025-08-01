77d07a2e1eeaba2942ef997e4e294b5cd9298b80
Kwon5an <gykwon0821@gmail.com>
2025-08-02 02:46:26 +0900

    Character 한글 깨짐 현상 수정

---
+++ Character.cpp
@@ -0,0 +1,35 @@
+#include "Character.h"
+#include <iostream>
+using namespace std;
+
+Character::Character(const string& name, int hp)
+	: name(name), hp(hp), maxHp(hp) {
+}
+
+void Character::takeDamage(int damage) {
+	hp -= damage;
+	if (hp < 0) hp = 0;
+	cout << name << "이/가 " << damage << "의 피해를 입었습니다! (현재 체력: " << hp << "/" << maxHp << ")" << endl;
+}
+
+void Character::heal(int amount) {
+	hp += amount;
+	if (hp > maxHp) hp = maxHp;
+	cout << name << "이/가 " << amount << "만큼 체력을 회복했습니다! (현재 체력: " << hp << "/" << maxHp << ")" << endl;
+}
+
+bool Character::isAlive() const {
+	return hp > 0;
+}
+
+string Character::getName() const {
+	return name;
+}
+
+int Character::getHp() const {
+	return hp;
+}
+
+int Character::getMaxHp() const {
+	return maxHp;
+}
